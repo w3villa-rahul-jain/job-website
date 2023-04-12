31f755b067052698c5580741f73c7ac4e83a8def
Rahul Jain <rahul.jain@w3villa.com>
2023-04-12 18:07:56 +0530

    add chat message functionlity

--- app/controllers/jobApplicants_controller.rb
+++ app/controllers/jobApplicants_controller.rb
@@ -1,8 +1,8 @@
 class JobApplicants < ApplicationController
 
-    def create
-        @user = current_user.id
-        @job = Job.find_by(params[:id]);
-        JobApplicant.create()
-    end
+    # def create
+    #     @user = current_user.id
+    #     @job = Job.find_by(params[:id]);
+    #     JobApplicant.create()
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :jobs
   devise_for :users
   resources :users
+  resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#index"
   post '/apply_jobs', to: 'jobs#apply_jobs', as: "apply_jobs"
