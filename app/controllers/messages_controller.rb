45f9577d1b914cf1e4b128a27685c4e35c83e246
Rahul Jain <rahul.jain@w3villa.com>
2023-04-13 19:28:45 +0530

    display chat message and some css style

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
 
-    def index
-        @messages = Room.find_by(id: params[:room_id]).messages
-    end
+    # def index
+    #     @messages = Room.find_by(id: params[:room_id]).messages
+    # end
     def create
       @current_user = current_user
       @message = @current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
